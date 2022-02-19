664ab98ac1b15f4c21ee825e6e45543bb230f664
LeonardMeagher2 <leonardmeagher2@gmail.com>
2022-02-19 08:55:38 -0800

    Auto exclude this addon from exporting

--- addons/github_to_itch/templates.gd
+++ addons/github_to_itch/templates.gd
@@ -41,14 +41,29 @@ static func get_exports() -> Array[Dictionary]:
 	var res:Array[Dictionary] = []
 	var config := ConfigFile.new()
 	config.load("res://export_presets.cfg")
+	var needs_saving = false
 	
 	for section in config.get_sections():
+		if config.has_section_key(section, "exclude_filter"):
+			# Set addon to be excluded
+			var exclude_filter:String = config.get_value(section, "exclude_filter", "")
+			var parts = exclude_filter.split(",", false)
+			if not "addons/github_to_itch/*" in parts:
+				parts.append("addons/github_to_itch/*")
+				exclude_filter = ",".join(parts)
+				config.set_value(section, "exclude_filter", exclude_filter)
+				needs_saving = true
+		
+			
 		if config.get_value(section, "runnable", false):
 			res.append({
 				name = config.get_value(section, "name"),
 				platform = config.get_value(section, "platform"),
 				export_path = config.get_value(section, "export_path"),
 			})
+	
+	if needs_saving:
+		config.save("res://export_presets.cfg")
 	
 	return res
 
